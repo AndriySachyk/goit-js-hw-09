2b3f8eaa925b2a41998c750605044d3b1cef91e5
AndriySachyk <119694775+AndriySachyk@users.noreply.github.com>
2023-05-17 21:21:53 +0000

    Deploying to gh-pages from @ AndriySachyk/goit-js-hw-09@a99059d3d9df4e0d26b68aed2d3f4519c5aea52c 🚀

---
+++ 01-color-switcher.f66583d8.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),d=document.body;let n=null;function a(){const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;d.style.backgroundColor=t}e.disabled=!0,e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(n)})),t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,n=setInterval(a,1e3)}));
+//# sourceMappingURL=01-color-switcher.f66583d8.js.map
